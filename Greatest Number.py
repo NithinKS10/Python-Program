80abfb89010ec2f00001c347fa6e9f94ab96f4b4
NithinKS10 <nithinksanthosh2001@gmail.com>
2024-09-11 20:25:20 +0530

    Program3

---
+++ Greatest Number.py
@@ -0,0 +1,8 @@
+a = int(input("Enter the First Number : "))
+b = int(input("Enter the Second Number : "))
+if a>b:
+    print("a is greater")
+elif a==b:
+    print("a and b are equal")
+else:
+    print("b is greater")
